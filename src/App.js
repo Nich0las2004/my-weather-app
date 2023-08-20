8374db00b94e1369fc96580c1992777ede5da563
Nika Oniani <nikaoniani2018@gmail.com>
2023-08-20 18:38:51 +0400

    fixed the issue

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import './App.css'
 
 const App = () => {
   const [info, setInfo] = useState()
+  const [btnClicked, setBtnClicked] = useState(false)
 
+  let latitude = 52.516266;
+  let longitude = 13.377775;
+  let apiKey = "bb3d2d98459078abb132df015a9e175f";
 
   const fetchHandler = async () => {
-    // brandenburg gate, germany
-
-    let latitude = 52.516266;
-    let longitude = 13.377775;
-    let apiKey = "bb3d2d98459078abb132df015a9e175f";
-    await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        setInfo(responseData)
-        console.log(responseData.sys)
-      });
+    try{
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+      )
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const responseData = await response.json();
+      setInfo(responseData);
+      console.log(responseData);
+      
+      setBtnClicked(true)
+    }
+    catch(err){
+      console.log(err)
+    }
   };
 
   return (
     <div>
-      <h3>Country: {info.sys.country}</h3>
+      {btnClicked && <h3>Country: {info.sys.country}</h3>}
       <button onClick={fetchHandler}>Fetch</button>
     </div>
   );
